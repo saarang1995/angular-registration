c8d42312a57c0d6c91143ba632d0f92014efd882
Saarang Tiwari <saarang@Saarangs-MacBook-Pro.local>
2019-03-13 03:27:33 +0530

    Added home module ,component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: "./modules/home.module#HomeModule",
+    data: {
+      preload: true
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from '../components/home/home.component';
+import { CommonComponentsModule } from './common-components.module';
+
+const appRoutes: Routes = [{ path: "", component: HomeComponent }];
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  exports: [HomeComponent],
+  imports: [
+    CommonModule,
+    CommonComponentsModule,
+    RouterModule.forChild(appRoutes)
+  ]
+})
+export class HomeModule { }
