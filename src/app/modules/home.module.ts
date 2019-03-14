f95b2b7af259126e8b372eeb62018ec734d0b951
Saarang Tiwari <saarang@Saarangs-MacBook-Pro.local>
2019-03-15 00:29:49 +0530

    Added hero component

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "../components/home/home.component";
 import { CommonComponentsModule } from "./common-components.module";
+import { HeroComponent } from '../components/hero/hero.component';
 
 const appRoutes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent],
   exports: [HomeComponent],
   imports: [
     CommonModule,
