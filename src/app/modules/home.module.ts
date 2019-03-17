3e198a77c9481b369f72208b02e1a76e8a4c174a
Saarang Tiwari <saarang@Saarangs-MacBook-Pro.local>
2019-03-17 11:13:12 +0530

    Added home body (empty)

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "../components/home/home.component";
 import { CommonComponentsModule } from "./common-components.module";
 import { HeroComponent } from '../components/hero/hero.component';
+import { HomeBodyComponent } from '../components/home-body/home-body.component';
 
 const appRoutes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent],
+  declarations: [HomeComponent, HeroComponent, HomeBodyComponent],
   exports: [HomeComponent],
   imports: [
     CommonModule,
